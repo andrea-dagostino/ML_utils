bbe4124626e9516eaf8a1683fd1eab2e92d303e8
Andrea D'Agostino <gothicfan100@gmail.com>
2021-08-20 17:21:25 +0200

    docs

--- visualization/eda.py
+++ visualization/eda.py
@@ -1,12 +1,18 @@
 def create_train_test_hists(x_train, x_test):
+    """Plots histograms for column comparison bewteen the train and test set. Useful to find inbalances in the dataset.
+
+    Args:
+        x_train ([type]): [description]
+        x_test ([type]): [description]
+    """
     columns = x_train.columns
 
-    plt.figure(figsize=(10,100))
-    for i,col in enumerate(columns[:-1]):
-        plt.subplot(34,3,i+1)
-        plt.hist(x=x_train[col],bins=25,color='teal', label="train")
-        plt.hist(x=x_test[col],bins=25,color='azure',alpha=0.5, label="test")
-        plt.title(f'{col}')
+    plt.figure(figsize=(10, 100))
+    for i, col in enumerate(columns[:-1]):
+        plt.subplot(34, 3, i + 1)
+        plt.hist(x=x_train[col], bins=25, color="teal", label="train")
+        plt.hist(x=x_test[col], bins=25, color="azure", alpha=0.5, label="test")
+        plt.title(f"{col}")
         plt.xticks(rotation=45)
         plt.legend()
     plt.tight_layout()
